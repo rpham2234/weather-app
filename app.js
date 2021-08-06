321180f2f551759c70fd390662a96eb2a81bae59
Richard Pham <richardvanpham@outlook.com>
2021-08-06 09:28:29 -0700

    search bar finally works

--- app.js
+++ app.js
@@ -6,45 +6,57 @@ app.engine('handlebars', exphbs({ defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
 var axios = require("axios").default;
-
-var options = {
-  method: 'GET',
-  url: 'https://community-open-weather-map.p.rapidapi.com/weather',
-  params: {
-    q: 'London,uk',
-    lat: '0',
-    lon: '0',
-    //callback: 'test',
-    id: '2172797',
-    lang: 'null',
-    units: 'metric',
-    mode: 'xml, html'
-  },
-  headers: {
-    'x-rapidapi-key': '9617f3f897msh7d7a21fbf5a4059p1d9ba6jsn7115cdc24774',
-    'x-rapidapi-host': 'community-open-weather-map.p.rapidapi.com'
-  }
-};
-
 var data;
-
-axios.request(options).then(function (response) {
-	console.log(response.data);
-  data = response.data;
-  console.log("Data variable" + data)
-}).catch(function (error) {
-	console.error(error);
-});
-
-axios.request(options).then(function (response) {
-	console.log(response.data);
-}).catch(function (error) {
-	console.error(error);
-});
-
+var format;
+var location;
 //routes
+
+//this is supposed to be the main page, and it needs parameters.
 app.get('/', (req, res) => {
+  //req.query
+  //const params = res.json(req.query)
+  location = req.query.loc
+  units = req.query.deg
+  if (units == "c"){ //User decides if they want Celsius or Farenheit. Default is Celsius.
+    format = "metric"
+  } else if(units == "f") {
+    format = "imperial"
+  } 
+  var options = {
+    method: 'GET',
+    url: 'https://community-open-weather-map.p.rapidapi.com/weather',
+    params: {
+      q: location,
+      lat: '0',
+      lon: '0',
+      //callback: 'test',
+      id: '2172797',
+      lang: 'null',
+      units: format,
+      mode: 'xml, html'
+    },
+    headers: {
+      'x-rapidapi-key': '9617f3f897msh7d7a21fbf5a4059p1d9ba6jsn7115cdc24774',
+      'x-rapidapi-host': 'community-open-weather-map.p.rapidapi.com'
+    }
+  };
+  
+
+  
+  axios.request(options).then(function (response) {
+    console.log(response.data);
+    data = response.data;
     res.render("home", {data: data})
+    console.log(("search").val())
+    console.log("Data variable" + data)
+  }).catch(function (error) {
+    console.error(error);
+  });
+    
+})
+
+app.get("/about", (req, res) => {
+  res.render("about", {})
 })
 
 app.listen(port, (req, res) => {
